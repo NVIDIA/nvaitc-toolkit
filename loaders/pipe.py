b6ec7ce914c40ad12fcba46a2a05d3c0f43238c4
Giuseppe Fiameni <gfiameni@nvidia.com>
2020-11-06 11:25:42 +0200

    new dirs structure

---
+++ loaders/pipe.py
@@ -0,0 +1,112 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2020 NVIDIA CORPORATION.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from nvidia.dali.pipeline import Pipeline
+from nvidia.dali.plugin.pytorch import DALIGenericIterator as PyTorchIterator
+from torch.utils.data import DataLoader
+from torchvision.datasets import STL10
+from PIL import Image
+
+import numpy as np
+import nvidia.dali.ops as ops
+import nvidia.dali.types as types
+import torchvision
+
+import horovod.torch as hvd
+
+import os
+import sys
+
+from random import shuffle
+
+class ImageNetTrainPipe(Pipeline):
+    def __init__(self, batch_size, num_threads, device_id, data_dir, crop, 
+                 shard_id, num_shards, dali_cpu=False):
+        super(ImageNetTrainPipe, self).__init__(batch_size,
+                                              num_threads,
+                                              device_id,
+                                              seed=12 + device_id)
+        self.input = ops.FileReader(file_root=data_dir,
+                                    shard_id=device_id,
+                                    num_shards=num_shards,
+                                    random_shuffle=True,
+                                    pad_last_batch=True)
+        #let user decide which pipeline works him bets for RN version he runs
+        dali_device = 'cpu' if dali_cpu else 'gpu'
+        decoder_device = 'cpu' if dali_cpu else 'mixed'
+        # This padding sets the size of the internal nvJPEG buffers to be able to 
+        # handle all images from full-sized ImageNet without additional reallocations
+        device_memory_padding = 211025920 if decoder_device == 'mixed' else 0
+        host_memory_padding = 140544512 if decoder_device == 'mixed' else 0
+        self.decode = ops.ImageDecoderRandomCrop(device=decoder_device, output_type=types.RGB,
+                                                 device_memory_padding=device_memory_padding,
+                                                 host_memory_padding=host_memory_padding,
+                                                 random_aspect_ratio=[0.8, 1.25],
+                                                 random_area=[0.1, 1.0],
+                                                 num_attempts=100)
+        self.res = ops.Resize(device=dali_device, resize_x=crop, resize_y=crop, 
+                              interp_type=types.INTERP_TRIANGULAR)
+        
+        self.cmnp = ops.CropMirrorNormalize(device="gpu", dtype=types.FLOAT, 
+                                            output_layout=types.NCHW,
+                                            crop=(crop, crop),
+                                            mean=[0.485 * 255,0.456 * 255,0.406 * 255],
+                                            std=[0.229 * 255,0.224 * 255,0.225 * 255])
+        self.coin = ops.CoinFlip(probability=0.5)
+        print('DALI "{0}" variant'.format(dali_device))
+
+    def define_graph(self):
+        rng = self.coin()
+        self.jpegs, self.labels = self.input(name="Reader")
+        images = self.decode(self.jpegs)
+        images = self.res(images)
+        output = self.cmnp(images.gpu(), mirror=rng)
+        return [output, self.labels]
+
+class ImageNetValPipe(Pipeline):
+    def __init__(self, batch_size, num_threads, device_id, data_dir, crop,
+                 size, shard_id, num_shards):
+        super(ImageNetValPipe, self).__init__(batch_size,
+                                           num_threads,
+                                            device_id,
+                                            seed=12 + device_id)
+        self.input = ops.FileReader(file_root=data_dir,
+                                    shard_id=device_id,
+                                    num_shards=num_shards,
+                                    random_shuffle=False,
+                                    pad_last_batch=True)
+        self.decode = ops.ImageDecoder(device="mixed", output_type=types.RGB)
+        self.res = ops.Resize(device="gpu",
+                              resize_shorter=size,
+                              interp_type=types.INTERP_TRIANGULAR)
+        self.cmnp = ops.CropMirrorNormalize(device="gpu",
+                                            dtype=types.FLOAT,
+                                            output_layout=types.NCHW,
+                                            crop=(crop, crop),
+                                            mean=[0.485 * 255,0.456 * 255,0.406 * 255],
+                                            std=[0.229 * 255,0.224 * 255,0.225 * 255])
+
+    def define_graph(self):
+        self.jpegs, self.labels = self.input(name="Reader")
+        images = self.decode(self.jpegs)
+        images = self.res(images)
+        output = self.cmnp(images)
+        return [output, self.labels]
